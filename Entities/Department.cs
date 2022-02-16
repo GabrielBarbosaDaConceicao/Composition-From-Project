b73c230d96be2d0718a4d6b82b9c131934296733
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-02-16 11:39:35 -0300

    Creating the compositions

---
+++ Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Composition_From_Project.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
